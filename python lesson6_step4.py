b6cd6cb0411839e99c53403f4f90956dff18683c
Vesta <vesta@Vestas-MacBook-Air.local>
2022-02-03 08:55:10 +0300

    домашка по первому модулю

---
+++ python lesson6_step4.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import time
+
+link = "http://suninjuly.github.io/simple_form_find_task.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    text = str(math.ceil(math.pow(math.pi, math.e)*10000))
+
+    link = browser.find_element_by_link_text(text)
+    link.click()
+
+
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
